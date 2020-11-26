d12abe03485f978f07f1eb092de215ad228e7711
Owen Yap <owen.y.yuhan@gmail.com>
2020-11-26 18:45:38 +0800

    Finished the actions and views of features

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_050058) do
+ActiveRecord::Schema.define(version: 2020_11_26_060643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_11_26_050058) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "url"
   end
 
   create_table "doses", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts 'Clearing database...'
+Ingredient.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_doc = open(url).read
+ingredients_list = JSON.parse(ingredients_doc)
+ingredients_list['drinks'].each do |ingredient|
+  new_ingredient = Ingredient.new(name: ingredient['strIngredient1'])
+  new_ingredient.save
+  puts "#{ingredient['strIngredient1']} added! 🍸"
+end
+
+puts '⭐⭐⭐Done!⭐⭐⭐'
